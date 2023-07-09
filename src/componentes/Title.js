37a86eab0bb0d46e73a44056dbb9abb4a05937a9
Sbrites <Sebastianbrites.com>
2023-07-09 16:50:43 -0300

    comits

---
+++ src/componentes/Title.js
@@ -0,0 +1,11 @@
+// Traemos al CSS
+import './Title.css';
+import ''
+
+export default function Title ({}) {
+    // const nombre = "Eva";
+
+    return (
+        <h1 className="title">¡Bienvenido A Mundo Pokemon {}!</h1>
+    )
+}
